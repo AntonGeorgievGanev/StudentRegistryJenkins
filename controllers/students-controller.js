e5dc3b069d1631a7436c3706e5148d81a2d496ff
Anton Ganev <122820049+AntonGeorgievGanev@users.noreply.github.com>
2024-10-15 06:55:44 +0300

    fix title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "DevOps Registry Test",
+      msg: "DevOps Registry",
       students: students
     };
     res.render('home', model);
